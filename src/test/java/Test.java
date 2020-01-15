4c8f53109d6967be4947c67f40d10fd51babbfb1
robolitan <robolitan1@yandex.ru>
2020-01-15 21:53:04 +0300

    change for test

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) throws SQLException {
+        System.out.println("good day");
 //        User artem = new User(0, "Artem", "1234", LocalDate.parse("1992-02-02"), Gender.MALE);
 //        User artem2 = new User(0, "Denis", "123789", LocalDate.parse("1992-09-11"), Gender.MALE);
 //        UserService userService = new UserService();
